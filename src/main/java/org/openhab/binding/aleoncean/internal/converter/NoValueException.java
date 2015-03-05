9b0d16db2e60ae651d7a62a15bd1beafd8de9864
Markus Rathgeb <maggu2810@gmail.com>
2015-03-05 15:58:40 +0100

    add missing serial version

--- src/main/java/org/openhab/binding/aleoncean/internal/converter/NoValueException.java
+++ src/main/java/org/openhab/binding/aleoncean/internal/converter/NoValueException.java
@@ -22,4 +22,9 @@ package org.openhab.binding.aleoncean.internal.converter;
  */
 public class NoValueException extends Exception {
 
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
 }
